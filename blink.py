1f3a8c1bfab8c9429274ad0ec9b5ffd446b78d1f
Lgun <463220275@qq.com>
2026-03-06 22:10:36 +0800

    feat: add fitst python probe script

---
+++ blink.py
@@ -0,0 +1,17 @@
+import time
+
+print("hello,树莓派！我是STM32老兵。")
+
+for i in raimport time
+
+print("Hello, 树莓派! 我是 STM32 老兵。")
+print("网络已修好，终端就是我的手术刀。")
+
+for i in range(5):
+    print(f"倒计时: {5-i}")
+    time.sleep(1)
+
+print("执行完毕！系统交回控制权。")print(f"倒计时：{5-i}")
+
+
+
